e4c6842d0fcefb44cc41cd05b372eb8348a14b65
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-21 00:11:05 +0500

    demonstrate how to create and acess the elements of a tuple using indexing

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# Tuples are similar to lists but are not mutable we wil cover this later for now
+# lets create a simple tumple in python!
+tuple1 = ("disco",10,1.2)
+print(tuple1)
+print(type(tuple1))
+# Each element of the tuple can also be accesed via index just like a list 
+print(f"Accessing the first element {tuple1[0]}")
+print(f"Accessing the second element {tuple1[1]}")
+print(f"Accessing the third element {tuple1[2]}")
