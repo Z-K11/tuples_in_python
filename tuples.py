de7cf4c376187cf074a72f02d931ba391d4e9417
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-21 00:15:02 +0500

    demonstrate how to acess the elements of a tuple using negative indexing

--- tuples.py
+++ tuples.py
@@ -7,3 +7,12 @@ print(type(tuple1))
 print(f"Accessing the first element {tuple1[0]}")
 print(f"Accessing the second element {tuple1[1]}")
 print(f"Accessing the third element {tuple1[2]}")
+print("We can also print out the type of each element inside a tuple ")
+print(f"Type of first element of the tuple {type(tuple1[0])}")
+print(f"Type of second element of the tuple {type(tuple1[1])}")
+print(f"Type of third element of the tuple {type(tuple1[2])}")
+# just like lists we can also use negative indexing to access the elements of a 
+# tuple 
+print(f"Accessing the first element of the tuple using negative index {tuple1[-3]}")
+print(f"Accessing the second element of the tuple using negative index {tuple1[-2]}")
+print(f"Accessing the third element of the tuple using negative index {tuple1[-1]}")
